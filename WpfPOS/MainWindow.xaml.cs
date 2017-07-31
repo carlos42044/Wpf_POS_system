c7d557721112d88ec92576ed9d2417b513332327
Carlos <carlos42044@gmail.com>
2017-07-31 14:30:47 -0400

    Completed the UI

--- WpfPOS/MainWindow.xaml.cs
+++ WpfPOS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AppSettingsLib;
 
 namespace WpfPOS
 {
@@ -20,25 +22,28 @@ namespace WpfPOS
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filename = @"C:\Users\CarlosF\Documents\Visual Studio 2017\Projects\WpfPOS\WpfPOS\settings.config";
 
-        public Visibility ButtonisVisible
+        Config config = new Config();
+
+        public Visibility ButtonIsVisible
         {
             get { return nameBtn.Visibility; }
             set { nameBtn.Visibility = value; }
         }
 
-        public Visibility LabelisVisible
+        public Visibility LabelIsVisible
         {
             get { return nameLabel.Visibility; }
             set { nameLabel.Visibility = value; }
         }
-        public Visibility ImageisVisible
+        public Visibility ImageIsVisible
         {
             get { return pictureBox1.Visibility; }
             set { pictureBox1.Visibility = value; }
         }
 
-        public Visibility DropisVisible
+        public Visibility DropIsVisible
         {
             get { return comboBox1.Visibility; }
             set { comboBox1.Visibility = value; }
@@ -47,12 +52,14 @@ namespace WpfPOS
         public MainWindow()
         {
             InitializeComponent();
+            containerVisibility();
         }
 
         private void nameBtn_Click(object sender, RoutedEventArgs e)
         {
             nameBtn.Content = "Clicked";
             nameBtn.IsEnabled = false;
+            //nameBtn.Visibility = Visibility.V
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -63,6 +70,29 @@ namespace WpfPOS
         private void nameLabel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             nameLabel.Content = "I have been gloriously clicked";
+        }
+
+        private void containerVisibility()
+        {
+            config.Read(filename);
+            ButtonIsVisible = config.Get("ButtonIsVisible");
+            NullToCollapsed.Convert(config.Get("ButtonIsVisible"), Visibility, null);
+            //ButtonIsVisible = ((Visibility)config.Get("ButtonIsVisible"));
+            //LabelIsVisible = ((Visibility)config.Get("LabelIsVisible"));
+            //ImageIsVisible = ((Visibility)config.Get("ImageIsVisible"));
+            //DropIsVisible = ((Visibility)config.Get("DropIsVisible"));
+        }
+    }
+
+    public static class NullToCollapsed : IValueConverter
+    {
+        public static object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public static object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
